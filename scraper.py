cf38e51e19ecd6bd38123dccda01d336c57e80e2
oshannnnn <oshanulusan@gmail.com>
2026-09-16 09:49:51 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,17 +9,27 @@ CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
 
 def get_news():
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
+   headers = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/131.0.0.0 Safari/537.36"
+    ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Referer": "https://neu.edu.tr/",
+    "Connection": "keep-alive",
+}
 
-    response = requests.get(
-        NEWS_URL,
-        headers=headers,
-        timeout=30
-    )
+response = requests.get(
+    NEWS_URL,
+    headers=headers,
+    timeout=30
+)
 
-    response.raise_for_status()
+print("HTTP durumu:", response.status_code)
+
+response.raise_for_status()
 
     soup = BeautifulSoup(response.text, "html.parser")
 
